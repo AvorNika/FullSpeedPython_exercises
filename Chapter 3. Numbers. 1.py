653cc5eb3a8ba0b4959e4024648edeaa9a1b8cd8
viktoriya <shakirova.vlg@gmail.com>
2023-06-19 13:04:32 +0500

    Added 4 problem solutions

---
+++ Chapter 3. Numbers. 1.py
@@ -0,0 +1,5 @@
+# Try the following mathematical calculations and guess what is happening:
+print(3 / 2, type(3 / 2))
+print(3 // 2, type(3 // 2))
+print(3 % 2, type(3 % 2))
+print(3 ** 2, type(3 ** 2))
---
+++ Chapter 3. Numbers. 4.py
@@ -0,0 +1,9 @@
+# Use the modulo operator (%) to check which of the following numbers is even or odd:
+numbers = (1, 5, 20, 60/7)
+for num in numbers:
+    if num % 2 == 0:
+        print(f'The number {num} is even')
+    elif num % 2 == 1:
+        print(f'The number {num} is odd')
+    else:
+        print(f'The number {num} is float')
